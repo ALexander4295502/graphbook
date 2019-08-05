3d6ffd74eebc2040b67aec9a0e8d5dd028555096
Zheng Yuan <yuanzheng@sportdex.io>
2019-08-04 21:24:29 -0700

    fix babel eslint parser issue and minor update for App components

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,61 @@
 import React, { Component } from 'react';
 
+import { USER_AVATAR_URL_PRESET } from './utils/Constant';
+
+const fakePosts = [
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      username: 'Test User 1',
+    },
+  },
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      username: 'Test User 2',
+    },
+  },
+];
+
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: fakePosts,
+    };
+  }
+
+  generateAvatarIdFromUsername = username => {
+    return username
+      .toLowerCase()
+      .split(' ')
+      .join('_');
+  };
+
   render() {
-    return <div>Hello World!</div>;
+    const { posts } = this.state;
+
+    return (
+      <div className="container">
+        <div className="feed">
+          {posts.map(post => (
+            <div key={post.id} className="post">
+              <div className="header">
+                <img
+                  src={`${USER_AVATAR_URL_PRESET}/${this.generateAvatarIdFromUsername(
+                    post.user.username,
+                  )}.png`}
+                  alt={post.user.username}
+                />
+                <h2>{post.user.username}</h2>
+              </div>
+              <p className="content">{post.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
---
+++ src/client/utils/Constant.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+const USER_AVATAR_URL_PRESET = 'https://api.adorable.io/avatars/100';
+
+export { USER_AVATAR_URL_PRESET };
