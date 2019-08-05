95f0d9e5c67279bb0e4737a316a07b601b69935b
Zheng Yuan <yuanzheng@sportdex.io>
2019-08-04 20:29:10 -0700

    minor update

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,41 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // eslint-disable-line import/no-extraneous-dependencies
-const CleanWebpackPlugin = require("clean-webpack-plugin"); // eslint-disable-line import/no-extraneous-dependencies
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 
-const buildDirectory = "dist";
+const buildDirectory = 'dist';
 const outputDirectory = `${buildDirectory}/client`;
 module.exports = {
-  mode: "development",
-  entry: "./src/client/index.js",
+  mode: 'development',
+  entry: './src/client/index.js',
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   devServer: {
     port: 3000,
-    open: true
+    open: true,
+    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPattern: [path.join(__dirname, buildDirectory)]
+      cleanOnceBeforeBuildPattern: [path.join(__dirname, buildDirectory)],
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
